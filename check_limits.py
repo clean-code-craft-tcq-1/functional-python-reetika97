2a2e259df38424cc93232bad02a8c268737ada71
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-03-01 15:53:01 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,6 +1,8 @@
 
 Battery_range= [[0,45], [20,80], [-1,0.8]]
+
 flag_range=[True, True, True]
+
 def check_ranges(param,high,lo):
   if param>high or param<lo
     return False
@@ -16,7 +18,7 @@ def battery_is_ok(temperature, soc, charge_rate):
   param=[temperature, soc, charge_rate]
   flag_range=[True, True, True]
   for i in range(0,3):
-    flag_range[i]=check_ranges(param[i],high[i],lo[i])
+    flag_range[i]=check_ranges(param[i],high[i][1],lo[i][0])
   return (Alert_OOR(flag_range)))
    
 
