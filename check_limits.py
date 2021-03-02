965a0ba679dc261631c6516c9697d0235b88e00e
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-03-02 16:04:09 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -43,9 +43,9 @@ if __name__ == '__main__':
   assert(battery_is_ok(50, 85, 0) is False)
   assert(battery_is_ok(50, 14, 0) is False)
   
- # assert(check_ranges(85,20,80)=='High' is True)
- # assert(check_ranges(15,20,80)=='Low' is True)
-#  assert(check_ranges(25,20,80)=='Normal' is True)
+  assert((check_ranges(85,20,80)=='High') is True)
+  assert((check_ranges(15,20,80)=='Low') is True)
+  assert((check_ranges(25,20,80)=='Normal') is True)
     
   assert((Breached_param(['Normal','High','Low'])=={'temperature':'Normal', 'soc':'High', 'charge_rate':'Low'}) is True)
   assert((Breached_param(['Normal','Normal','Normal'])=={'temperature':'Normal', 'soc':'Normal', 'charge_rate':'Normal'}) is True)
