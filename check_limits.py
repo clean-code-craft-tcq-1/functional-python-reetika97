e31e995d9b59adec490c45c4f4d9d9ebde766c5c
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-03-02 15:30:02 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -4,6 +4,7 @@ Battery_range= [[0,45], [20,80], [-sys.maxsize,0.8]]
 
 flag_range=[True, True, True]
 
+#Check For parameters within Range
 def check_ranges(param,high,lo):
   if param>high:
     return 'H'
@@ -11,13 +12,15 @@ def check_ranges(param,high,lo):
     return 'L'
   else: 
     return True
-  
+
+#Alert for deviations
 def Alert_OOR(flags):
   if 'H' in flags or 'L' in flags:
     return False
   else: 
     return True
   
+#Breach Report for Functions
 def Breached_param(flags):
   param=['temperature', 'soc', 'charge_rate']
   breach_report={'temperature':'Normal', 'soc':'Normal', 'charge_rate':'Normal'}
@@ -28,14 +31,16 @@ def Breached_param(flags):
       breach_report[param[i]]='Low'
       
   print(breach_report)
+  return breach_report
 
 def battery_is_ok(temperature, soc, charge_rate):
   param=[temperature, soc, charge_rate]
+  breach_report={'temperature':'Normal', 'soc':'Normal', 'charge_rate':'Normal'}
   flag_range=[True, True, True]
   for i in range(0,3):
     flag_range[i]=check_ranges(param[i],Battery_range[i][1],Battery_range[i][0])
   if Alert_OOR(flag_range)==False:
-    Breached_param(flag_range)
+    breach_report=Breached_param(flag_range)
   return (Alert_OOR(flag_range))
 
 if __name__ == '__main__':
